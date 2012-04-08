7ee352fd6ab890516f11d40f8f613e84d1c243eb
Justin Santa Barbara <justin@fathomdb.com>
2012-04-08 11:06:16 -0700

    Fix up JVM install

--- ops/core/src/main/java/org/platformlayer/ops/java/JavaVirtualMachine.java
+++ ops/core/src/main/java/org/platformlayer/ops/java/JavaVirtualMachine.java
@@ -13,82 +13,104 @@ import org.platformlayer.service.imagefactory.v1.Repository;
 
 public class JavaVirtualMachine extends OpsTreeBase implements HasDiskImageRecipe {
 
-    public String version;
+	public String version;
 
-    @Handler
-    public void handler() {
+	@Handler
+	public void handler() {
 
-    }
+	}
 
-    @Override
-    public void addTo(DiskImageRecipe recipe) {
-        if (version.equals("6")) {
-            Repository repository = new Repository();
-            repository.setKey("debian-non-free");
-            repository.getSource().add("deb http://ftp.us.debian.org/debian squeeze non-free");
-            recipe.getRepository().add(repository);
+	@Override
+	public void addTo(DiskImageRecipe recipe) {
+		if (version.equals("6")) {
+			Repository repository = new Repository();
+			repository.setKey("debian-non-free");
+			repository.getSource().add("deb http://ftp.us.debian.org/debian squeeze non-free");
+			recipe.getRepository().add(repository);
 
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jdk", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "sun-java6-jre/stopthread", "boolean", "true");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "sun-java6-jre/jcepolicy", "note", "");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/error-sun-dlj-v1-1", "error", "");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jdk", "shared/error-sun-dlj-v1-1", "error", "");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "shared/error-sun-dlj-v1-1", "error", "");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/present-sun-dlj-v1-1", "note", "");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jdk", "shared/present-sun-dlj-v1-1", "note", "");
-            DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "shared/present-sun-dlj-v1-1", "note", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/accepted-sun-dlj-v1-1", "boolean",
+					"true");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/accepted-sun-dlj-v1-1", "boolean",
+					"true");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jdk", "shared/accepted-sun-dlj-v1-1", "boolean",
+					"true");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "shared/accepted-sun-dlj-v1-1", "boolean",
+					"true");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "sun-java6-jre/stopthread", "boolean",
+					"true");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "sun-java6-jre/jcepolicy", "note", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/error-sun-dlj-v1-1", "error", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jdk", "shared/error-sun-dlj-v1-1", "error", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "shared/error-sun-dlj-v1-1", "error", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-bin", "shared/present-sun-dlj-v1-1", "note", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jdk", "shared/present-sun-dlj-v1-1", "note", "");
+			DiskImageRecipeBuilder.addPreconfigure(recipe, "sun-java6-jre", "shared/present-sun-dlj-v1-1", "note", "");
 
-            recipe.getAddPackage().add("sun-java6-jdk");
-            recipe.getAddPackage().add("sun-java6-jre");
-        } else if (version.equals("7")) {
-            OperatingSystemRecipe operatingSystem = recipe.getOperatingSystem();
-            if (operatingSystem == null) {
-                operatingSystem = new OperatingSystemRecipe();
-            }
+			recipe.getAddPackage().add("sun-java6-jdk");
+			recipe.getAddPackage().add("sun-java6-jre");
+		} else if (version.equals("7")) {
+			OperatingSystemRecipe operatingSystem = recipe.getOperatingSystem();
+			if (operatingSystem == null) {
+				operatingSystem = new OperatingSystemRecipe();
+			}
 
-            if (operatingSystem.getDistribution() == null) {
-                operatingSystem.setDistribution("debian");
-            }
+			if (operatingSystem.getDistribution() == null) {
+				operatingSystem.setDistribution("debian");
+			}
 
-            if (operatingSystem.getVersion() == null) {
-                if (operatingSystem.getDistribution().equalsIgnoreCase("debian")) {
-                    operatingSystem.setVersion("wheezy");
-                }
-            }
-            recipe.setOperatingSystem(operatingSystem);
-        } else {
-            throw new IllegalArgumentException("Unknown java version: " + version);
-        }
-    }
+			if (operatingSystem.getVersion() == null) {
+				if (operatingSystem.getDistribution().equalsIgnoreCase("debian")) {
+					operatingSystem.setVersion("wheezy");
+				}
+			}
+			recipe.setOperatingSystem(operatingSystem);
+		} else {
+			throw new IllegalArgumentException("Unknown java version: " + version);
+		}
+	}
 
-    @Override
-    protected void addChildren() throws OpsException {
-        if (version.equals("6")) {
-            addChild(PackageDependency.build("sun-java6-jdk"));
-            addChild(PackageDependency.build("sun-java6-jre"));
-        } else if (version.equals("7")) {
-            addChild(PackageDependency.build("openjdk-7-jre"));
-        } else {
-            throw new IllegalArgumentException("Unknown java version: " + version);
-        }
+	@Override
+	protected void addChildren() throws OpsException {
+		if (version.equals("6")) {
+			{
+				PackageDependency jre = PackageDependency.build("sun-java6-jre");
+				jre.addConfiguration("sun-java6-bin", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
+				jre.addConfiguration("sun-java6-bin", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
+				jre.addConfiguration("sun-java6-jdk", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
+				jre.addConfiguration("sun-java6-jre", "shared/accepted-sun-dlj-v1-1", "boolean", "true");
+				jre.addConfiguration("sun-java6-jre", "sun-java6-jre/stopthread", "boolean", "true");
+				jre.addConfiguration("sun-java6-jre", "sun-java6-jre/jcepolicy", "note", "");
+				jre.addConfiguration("sun-java6-bin", "shared/error-sun-dlj-v1-1", "error", "");
+				jre.addConfiguration("sun-java6-jdk", "shared/error-sun-dlj-v1-1", "error", "");
+				jre.addConfiguration("sun-java6-jre", "shared/error-sun-dlj-v1-1", "error", "");
+				jre.addConfiguration("sun-java6-bin", "shared/present-sun-dlj-v1-1", "note", "");
+				jre.addConfiguration("sun-java6-jdk", "shared/present-sun-dlj-v1-1", "note", "");
+				jre.addConfiguration("sun-java6-jre", "shared/present-sun-dlj-v1-1", "note", "");
 
-    }
+				addChild(jre);
+			}
 
-    public static JavaVirtualMachine build(String version) {
-        JavaVirtualMachine jvm = Injection.getInstance(JavaVirtualMachine.class);
-        jvm.version = version;
-        return jvm;
-    }
+			addChild(PackageDependency.build("sun-java6-jdk"));
+		} else if (version.equals("7")) {
+			addChild(PackageDependency.build("openjdk-7-jre"));
+		} else {
+			throw new IllegalArgumentException("Unknown java version: " + version);
+		}
 
-    public static JavaVirtualMachine buildJava6() {
-        return build("6");
-    }
+	}
 
-    public static JavaVirtualMachine buildJava7() {
-        return build("7");
-    }
+	public static JavaVirtualMachine build(String version) {
+		JavaVirtualMachine jvm = Injection.getInstance(JavaVirtualMachine.class);
+		jvm.version = version;
+		return jvm;
+	}
+
+	public static JavaVirtualMachine buildJava6() {
+		return build("6");
+	}
+
+	public static JavaVirtualMachine buildJava7() {
+		return build("7");
+	}
 
 }
