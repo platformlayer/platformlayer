92b2ebe90efdd786fad4212e3643c3bccc89b0a8
Justin Santa Barbara <justin@fathomdb.com>
2012-04-14 07:12:02 -0700

    Use new port numbers when stopping services based on port

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,10 +6,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${SCRIPT_DIR}/common
 
 #pkill -f keystone-webapp-user-${VERSION}.jar
-fuser -k -n tcp 5000 || true
+fuser -k -n tcp 5001 || true
 
 #pkill -f keystone-webapp-admin-${VERSION}.jar
-fuser -k -n tcp 35357 || true
+fuser -k -n tcp 35358 || true
 
 #pkill -f platformlayer-xaas-webapp-${VERSION}.jar
 fuser -k -n tcp 8082 || true
