3a53b3acef7788baeb364eedfd39418e81ebf947
Justin Santa Barbara <justin@fathomdb.com>
2012-04-14 08:45:26 -0700

    Fix job_logs item-key column type int -> text

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -3,15 +3,17 @@
 set -e
 
 function run_as_postgres() {
+	DB="$1"
+
 	case $OSTYPE in
-		darwin*) $1 ;;
-		*) su - postgres -c "$1";;
+		darwin*) psql --host 127.0.0.1 --db ${DB} ;;
+		*) su - postgres -c "psql --db ${DB}";;
 	esac
 }
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR=${SCRIPT_DIR}/../
 
-cat ${BASE_DIR}/db/createdb.sql | run_as_postgres "psql --host 127.0.0.1 --db template1"
-cat ${BASE_DIR}/db/schema.sql | run_as_postgres "psql --host 127.0.0.1 --db platformlayer"
+cat ${BASE_DIR}/db/createdb.sql | run_as_postgres template1
+cat ${BASE_DIR}/db/schema.sql | run_as_postgres platformlayer
 
