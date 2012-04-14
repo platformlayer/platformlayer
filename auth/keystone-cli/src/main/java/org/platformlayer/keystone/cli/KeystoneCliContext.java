07d63435fda525447fc9847df7ff289e8270b1e5
Justin Santa Barbara <justin@fathomdb.com>
2012-04-14 10:43:01 -0700

    Throw better error message if not configured properly; reformat

--- auth/keystone-cli/src/main/java/org/platformlayer/keystone/cli/KeystoneCliContext.java
+++ auth/keystone-cli/src/main/java/org/platformlayer/keystone/cli/KeystoneCliContext.java
@@ -8,43 +8,49 @@ import org.platformlayer.keystone.cli.formatters.KeystoneFormatterRegistry;
 import org.platformlayer.keystone.cli.guice.CliModule;
 
 import com.fathomdb.cli.CliContextBase;
+import com.fathomdb.cli.CliException;
+import com.google.inject.ConfigurationException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class KeystoneCliContext extends CliContextBase {
-    final KeystoneCliOptions options;
-    private Injector injector;
+	final KeystoneCliOptions options;
+	private Injector injector;
 
-    public KeystoneCliContext(KeystoneCommandRegistry commandRegistry, KeystoneCliOptions options) {
-        super(commandRegistry, new KeystoneFormatterRegistry());
-        this.options = options;
-    }
+	public KeystoneCliContext(KeystoneCommandRegistry commandRegistry, KeystoneCliOptions options) {
+		super(commandRegistry, new KeystoneFormatterRegistry());
+		this.options = options;
+	}
 
-    @Override
-    public void connect() throws Exception {
-        this.injector = Guice.createInjector(new CliModule(options));
-    }
+	@Override
+	public void connect() throws Exception {
+		this.injector = Guice.createInjector(new CliModule(options));
+	}
 
-    public UserRepository getUserRepository() {
-        return injector.getInstance(UserRepository.class);
-    }
+	public UserRepository getUserRepository() {
+		try {
+			return injector.getInstance(UserRepository.class);
+		} catch (ConfigurationException e) {
+			throw new CliException("Database not configured; must set auth.system.module in configuration");
+		}
+	}
 
-    public OpsUser login() throws RepositoryException {
-        String username = options.getUsername();
-        String password = options.getPassword();
-        if (username == null || password == null) {
-            throw new IllegalArgumentException("Must specify username & password");
-        }
-        OpsUser user = getUserRepository().findUser(username);
-        if (user != null) {
-            if (!user.isPasswordMatch(password)) {
-                user = null;
-            }
-        }
-        if (user == null) {
-            throw new SecurityException("Credentials were not valid");
-        }
-        user.unlockWithPassword(password);
-        return user;
-    }
+	public OpsUser login() throws RepositoryException {
+		String username = options.getUsername();
+		String password = options.getPassword();
+		if (username == null || password == null) {
+			throw new IllegalArgumentException("Must specify username & password");
+		}
+		OpsUser user = getUserRepository().findUser(username);
+		if (user != null) {
+			if (!user.isPasswordMatch(password)) {
+				user = null;
+			}
+		}
+		if (user == null) {
+			throw new SecurityException("Credentials were not valid");
+		}
+		user.unlockWithPassword(password);
+		return user;
+	}
 }
