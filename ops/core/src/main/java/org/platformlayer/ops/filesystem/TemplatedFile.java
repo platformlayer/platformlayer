ee60475f6dc5abfdaf088af08927b4f384b66de6
Justin Santa Barbara <justin@fathomdb.com>
2012-04-14 07:17:37 -0700

    Allow TemplatedFile to use a class-relative resource name with the explicit override; reformat

--- ops/core/src/main/java/org/platformlayer/ops/filesystem/TemplatedFile.java
+++ ops/core/src/main/java/org/platformlayer/ops/filesystem/TemplatedFile.java
@@ -15,131 +15,136 @@ import org.platformlayer.ops.templates.TemplateEngine;
 import com.google.common.collect.Maps;
 
 public class TemplatedFile extends SyntheticFile {
-    static final Logger log = Logger.getLogger(TemplatedFile.class);
+	static final Logger log = Logger.getLogger(TemplatedFile.class);
 
-    @Inject
-    TemplateEngine template;
+	@Inject
+	TemplateEngine template;
 
-    String templateName;
+	String templateName;
 
-    TemplateDataSource templateDataSource;
+	TemplateDataSource templateDataSource;
 
-    @Override
-    protected byte[] getContentsBytes() throws OpsException {
-        Map<String, Object> model = buildModel();
-        String templateString = template.runTemplateToString(getTemplateName(), model);
-        return Utf8.getBytes(templateString);
-    }
+	@Override
+	protected byte[] getContentsBytes() throws OpsException {
+		Map<String, Object> model = buildModel();
+		String templateString = template.runTemplateToString(getTemplateName(), model);
+		return Utf8.getBytes(templateString);
+	}
 
-    protected Map<String, Object> buildModel() throws OpsException {
-        Map<String, Object> model = Maps.newHashMap();
-        TemplateDataSource templateDataSource = getTemplateDataSource();
-        if (templateDataSource != null) {
-            templateDataSource.buildTemplateModel(model);
-        }
-        return model;
-    }
+	protected Map<String, Object> buildModel() throws OpsException {
+		Map<String, Object> model = Maps.newHashMap();
+		TemplateDataSource templateDataSource = getTemplateDataSource();
+		if (templateDataSource != null) {
+			templateDataSource.buildTemplateModel(model);
+		}
+		return model;
+	}
 
-    public String getTemplateName() {
-        return templateName;
-    }
+	public String getTemplateName() {
+		return templateName;
+	}
 
-    public void setTemplateName(String templateName) {
-        this.templateName = templateName;
-    }
+	public void setTemplateName(String templateName) {
+		this.templateName = templateName;
+	}
 
-    public static TemplatedFile build(TemplateDataSource context, File filePath, String templateName) {
-        TemplatedFile templatedFile = Injection.getInstance(TemplatedFile.class);
-        templatedFile.setTemplateName(templateName);
-        templatedFile.filePath = filePath;
-        templatedFile.setTemplateDataSource(context);
-        return templatedFile;
-    }
+	public static TemplatedFile build(TemplateDataSource context, File filePath, String templateName) {
+		String templatePath = templateName;
+		if (!templatePath.contains("/")) {
+			templatePath = getDefaultResourceName(context.getClass(), templatePath);
+		}
 
-    public static TemplatedFile build(TemplateDataSource context, File filePath) {
-        String templateName = getDefaultResourceName(context.getClass(), filePath);
-        return build(context, filePath, templateName);
-    }
+		TemplatedFile templatedFile = Injection.getInstance(TemplatedFile.class);
+		templatedFile.setTemplateName(templatePath);
+		templatedFile.filePath = filePath;
+		templatedFile.setTemplateDataSource(context);
+		return templatedFile;
+	}
 
-    public TemplateDataSource getTemplateDataSource() {
-        return templateDataSource;
-    }
+	public static TemplatedFile build(TemplateDataSource context, File filePath) {
+		String templateName = getDefaultResourceName(context.getClass(), filePath);
+		return build(context, filePath, templateName);
+	}
 
-    public void setTemplateDataSource(TemplateDataSource templateDataSource) {
-        this.templateDataSource = templateDataSource;
-    }
+	public TemplateDataSource getTemplateDataSource() {
+		return templateDataSource;
+	}
 
-    // public static void buildDefaultContext(Map<String, Object> context, OpsItem opsItem) {
-    // context.put("me", opsItem);
-    // context.put("ops", opsItem.getOpsSystem());
-    //
-    // // Put the entire chain in, keyed by their type name
-    // OpsNode current = opsItem;
-    // while (current != null) {
-    // Class<?> currentClass = current.getClass();
-    // do {
-    // String key = currentClass.getSimpleName();
-    // if (key.length() != 0) {
-    // key = Character.toLowerCase(key.charAt(0)) + key.substring(1);
-    // if (!context.containsKey(key))
-    // context.put(key, current);
-    // }
-    // currentClass = currentClass.getSuperclass();
-    // } while (!currentClass.equals(OpsItem.class));
-    //
-    // current = current.getParent();
-    // }
-    //
-    // OpsServer server = opsItem.smartGetServer(false);
-    // if (server == null) {
-    // log.info("Warning - opsServer is null for template");
-    // }
-    // context.put("server", server);
-    // context.put("opsServer", server);
-    // }
-    //
-    // protected void buildContext(Map<String, Object> context) {
-    // buildDefaultContext(context, this);
-    // }
-    //
-    // public static String executeTemplate(OpsSystem opsSystem, String templateFile, final OpsItem item) throws
-    // TemplateException {
-    // return executeTemplate(opsSystem, templateFile, new Functor<Map<String, Object>>() {
-    // public void invoke(Map<String, Object> context) {
-    // buildDefaultContext(context, item);
-    // }
-    // });
-    // }
-    //
-    // public static String executeTemplate(OpsSystem opsSystem, String templateFile, Functor<Map<String, Object>>
-    // contextBuilder) throws TemplateException {
-    // // Accept (but do not require) res: prefixed files
-    // if (templateFile.startsWith("res:"))
-    // templateFile = templateFile.substring(4);
-    //
-    // TemplateApplication template = opsSystem.getTemplateApplication(templateFile, contextBuilder);
-    // return template.runTemplateToString();
-    // }
-    //
-    // protected String executeTemplate() throws TemplateException {
-    // // Accept (but do not require) res: prefixed files
-    // String templateFile = getSourceFile();
-    // return executeTemplate(getOpsSystem(), templateFile, this);
-    // }
-    //
-    // @Override
-    // // @DisplayAnnotation(shouldDump = false)
-    // public String getContents() throws OpsException {
-    // try {
-    // return executeTemplate();
-    // } catch (TemplateException e) {
-    // throw new OpsException("Could not build templated file", e);
-    // }
-    // }
-    //
-    // @Override
-    // public void setContents(String contents) {
-    // throw new IllegalStateException();
-    // }
+	public void setTemplateDataSource(TemplateDataSource templateDataSource) {
+		this.templateDataSource = templateDataSource;
+	}
+
+	// public static void buildDefaultContext(Map<String, Object> context, OpsItem opsItem) {
+	// context.put("me", opsItem);
+	// context.put("ops", opsItem.getOpsSystem());
+	//
+	// // Put the entire chain in, keyed by their type name
+	// OpsNode current = opsItem;
+	// while (current != null) {
+	// Class<?> currentClass = current.getClass();
+	// do {
+	// String key = currentClass.getSimpleName();
+	// if (key.length() != 0) {
+	// key = Character.toLowerCase(key.charAt(0)) + key.substring(1);
+	// if (!context.containsKey(key))
+	// context.put(key, current);
+	// }
+	// currentClass = currentClass.getSuperclass();
+	// } while (!currentClass.equals(OpsItem.class));
+	//
+	// current = current.getParent();
+	// }
+	//
+	// OpsServer server = opsItem.smartGetServer(false);
+	// if (server == null) {
+	// log.info("Warning - opsServer is null for template");
+	// }
+	// context.put("server", server);
+	// context.put("opsServer", server);
+	// }
+	//
+	// protected void buildContext(Map<String, Object> context) {
+	// buildDefaultContext(context, this);
+	// }
+	//
+	// public static String executeTemplate(OpsSystem opsSystem, String templateFile, final OpsItem item) throws
+	// TemplateException {
+	// return executeTemplate(opsSystem, templateFile, new Functor<Map<String, Object>>() {
+	// public void invoke(Map<String, Object> context) {
+	// buildDefaultContext(context, item);
+	// }
+	// });
+	// }
+	//
+	// public static String executeTemplate(OpsSystem opsSystem, String templateFile, Functor<Map<String, Object>>
+	// contextBuilder) throws TemplateException {
+	// // Accept (but do not require) res: prefixed files
+	// if (templateFile.startsWith("res:"))
+	// templateFile = templateFile.substring(4);
+	//
+	// TemplateApplication template = opsSystem.getTemplateApplication(templateFile, contextBuilder);
+	// return template.runTemplateToString();
+	// }
+	//
+	// protected String executeTemplate() throws TemplateException {
+	// // Accept (but do not require) res: prefixed files
+	// String templateFile = getSourceFile();
+	// return executeTemplate(getOpsSystem(), templateFile, this);
+	// }
+	//
+	// @Override
+	// // @DisplayAnnotation(shouldDump = false)
+	// public String getContents() throws OpsException {
+	// try {
+	// return executeTemplate();
+	// } catch (TemplateException e) {
+	// throw new OpsException("Could not build templated file", e);
+	// }
+	// }
+	//
+	// @Override
+	// public void setContents(String contents) {
+	// throw new IllegalStateException();
+	// }
 
 }
