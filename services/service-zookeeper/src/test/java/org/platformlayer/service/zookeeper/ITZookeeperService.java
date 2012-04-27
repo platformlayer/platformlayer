005309adc816263b10b24a69aedbb692128ce5e0
Justin Santa Barbara <justin@fathomdb.com>
2012-04-27 10:46:52 -0700

    Added test for zookeeper service

---
+++ services/service-zookeeper/src/test/java/org/platformlayer/service/zookeeper/ITZookeeperService.java
@@ -0,0 +1,56 @@
+package org.platformlayer.service.zookeeper;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.platformlayer.IoUtils;
+import org.platformlayer.service.zookeeper.model.ZookeeperCluster;
+import org.platformlayer.tests.PlatformLayerApiTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ITZookeeperService extends PlatformLayerApiTest {
+
+	@Override
+	@BeforeMethod
+	public void beforeMethod() {
+		super.beforeMethod();
+
+		getTypedItemMapper().addClass(ZookeeperCluster.class);
+	}
+
+	@Test
+	public void testCreateAndDeleteItem() throws Exception {
+		String id = random.randomAlphanumericString(8);
+
+		ZookeeperCluster cluster = new ZookeeperCluster();
+		cluster.dnsName = id + ".test.platformlayer.org";
+
+		cluster = putItem(id, cluster);
+		cluster = waitForHealthy(cluster);
+
+		InetSocketAddress socketAddress = getEndpoint(cluster);
+		Assert.assertFalse(isPortOpen(socketAddress));
+
+		openFirewall(cluster, socketAddress.getPort());
+		Assert.assertTrue(isPortOpen(socketAddress));
+
+		String response = testZookeeper(socketAddress);
+		response = response.trim();
+		Assert.assertTrue(response.contains("imok"));
+	}
+
+	private String testZookeeper(InetSocketAddress socketAddress) throws IOException {
+		Socket socket = new Socket();
+		socket.connect(socketAddress);
+		socket.getOutputStream().write("ruok\n".getBytes());
+
+		String response = IoUtils.readAll(socket.getInputStream());
+
+		socket.close();
+
+		return response;
+	}
+}
