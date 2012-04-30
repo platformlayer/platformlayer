666d6159e2662d163882bede43207c775fe4b0a8
Justin Santa Barbara <justin@fathomdb.com>
2012-04-30 09:46:42 -0700

    Default name of GitRepo to the id

--- services/service-git/src/main/java/org/platformlayer/service/git/GitProvider.java
+++ services/service-git/src/main/java/org/platformlayer/service/git/GitProvider.java
@@ -1,9 +1,28 @@
 package org.platformlayer.service.git;
 
+import org.platformlayer.core.model.ItemBase;
+import org.platformlayer.ops.OpsException;
 import org.platformlayer.ops.ServiceProviderBase;
+import org.platformlayer.service.git.model.GitRepository;
 import org.platformlayer.xaas.Service;
+
+import com.google.common.base.Strings;
 
 @Service("git")
 public class GitProvider extends ServiceProviderBase {
+
+	@Override
+	public void beforeCreateItem(ItemBase item) throws OpsException {
+		super.beforeCreateItem(item);
+
+		// TODO: This doesn't feel like the right place for this
+		if (item instanceof GitRepository) {
+			GitRepository repo = (GitRepository) item;
+
+			if (Strings.isNullOrEmpty(repo.name)) {
+				repo.name = repo.getId();
+			}
+		}
+	}
 
 }
