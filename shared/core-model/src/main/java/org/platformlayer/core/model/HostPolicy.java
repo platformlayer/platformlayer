a6c198f9ef85023fb4d70e922916b1a428385999
Justin Santa Barbara <justin@fathomdb.com>
2012-05-01 21:22:33 -0700

    Change default for allowRunInContainer to be true

--- shared/core-model/src/main/java/org/platformlayer/core/model/HostPolicy.java
+++ shared/core-model/src/main/java/org/platformlayer/core/model/HostPolicy.java
@@ -7,23 +7,5 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
 public class HostPolicy {
-	public boolean allowRunInContainer;
-	//
-	// private static final String KEY_ALLOW_RUN_IN_CONTAINER = "container";
-	// List<String> keys = Lists.newArrayList();
-	//
-	// public boolean isCanRunInContainer() {
-	// return keys.contains(KEY_ALLOW_RUN_IN_CONTAINER);
-	// }
-	//
-	// public void allowRunInContainer() {
-	// if (!keys.contains(KEY_ALLOW_RUN_IN_CONTAINER)) {
-	// keys.add(KEY_ALLOW_RUN_IN_CONTAINER);
-	// }
-	// }
-	//
-	// public Tag buildTag() {
-	// String serialized = Joiner.on(";").join(keys);
-	// return new Tag(Tag.HOST_POLICY, serialized);
-	// }
+	public boolean allowRunInContainer = true;
 }
