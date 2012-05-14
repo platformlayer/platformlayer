311435733d802bce283955190b624aff79e5dc81
Justin Santa Barbara <justin@fathomdb.com>
2012-05-13 18:18:30 -0700

    Run dpkg --configure -a if apt broke previously

--- ops/core/src/main/java/org/platformlayer/ops/helpers/AptHelper.java
+++ ops/core/src/main/java/org/platformlayer/ops/helpers/AptHelper.java
@@ -10,9 +10,13 @@ import org.apache.log4j.Logger;
 import org.platformlayer.TimeSpan;
 import org.platformlayer.ops.Command;
 import org.platformlayer.ops.CommandEnvironment;
+import org.platformlayer.ops.FileUpload;
 import org.platformlayer.ops.OpsException;
 import org.platformlayer.ops.OpsTarget;
-import org.platformlayer.ops.helpers.HttpProxyHelper.Usage;
+import org.platformlayer.ops.process.ProcessExecution;
+import org.platformlayer.ops.process.ProcessExecutionException;
+import org.platformlayer.ops.proxy.HttpProxyHelper;
+import org.platformlayer.ops.proxy.HttpProxyHelper.Usage;
 import org.platformlayer.service.imagefactory.v1.ConfigurePackage;
 
 public class AptHelper {
@@ -42,7 +46,7 @@ public class AptHelper {
 		if (!haveCurl(target)) {
 			log.warn("We don't yet have curl; can't detect best proxy so won't use a proxy");
 		} else {
-			commandEnvironment = httpProxies.getHttpProxyEnvironment(target, Usage.SoftwarePackages);
+			commandEnvironment = httpProxies.getHttpProxyEnvironment(target, Usage.SoftwarePackages, null);
 		}
 
 		if (commandEnvironment == null) {
@@ -56,7 +60,38 @@ public class AptHelper {
 		CommandEnvironment commandEnvironment = buildEnvironmentWithProxy(target);
 
 		Command command = Command.build("apt-get --yes update");
-		target.executeCommand(command.setEnvironment(commandEnvironment).setTimeout(TimeSpan.TEN_MINUTES));
+		command = command.setEnvironment(commandEnvironment).setTimeout(TimeSpan.TEN_MINUTES);
+		executeAptCommand(target, command);
+	}
+
+	private void executeAptCommand(OpsTarget target, Command command) throws OpsException {
+		for (int attempt = 0; attempt < 2; attempt++) {
+			try {
+				target.executeCommand(command);
+			} catch (ProcessExecutionException e) {
+				if (attempt == 0) {
+					ProcessExecution execution = e.getExecution();
+					String stdErr = execution.getStdErr();
+					// Stderr=E: dpkg was interrupted, you must manually run 'dpkg --configure -a' to correct the
+					// problem.
+					if (stdErr.contains("dpkg --configure -a")) {
+						log.warn("Detected that dpkg --configure -a is needed");
+						doDpkgConfigure(target);
+						log.warn("Retrying apt command");
+						continue;
+					}
+				}
+				throw new OpsException("Error executing apt command", e);
+			}
+			return;
+		}
+	}
+
+	private void doDpkgConfigure(OpsTarget target) throws OpsException {
+		CommandEnvironment commandEnvironment = buildEnvironmentWithProxy(target);
+		Command command = Command.build("dpkg --configure -a");
+		command = command.setEnvironment(commandEnvironment).setTimeout(TimeSpan.TEN_MINUTES);
+		target.executeCommand(command);
 	}
 
 	public void upgrade(OpsTarget target) throws OpsException {
