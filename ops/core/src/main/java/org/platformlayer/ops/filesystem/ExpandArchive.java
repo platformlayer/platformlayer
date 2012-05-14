1470dd7871b4eaf52c9844b013f07d9a74cab590
Justin Santa Barbara <justin@fathomdb.com>
2012-05-13 18:30:40 -0700

    Clean up ExpandArchive

--- ops/core/src/main/java/org/platformlayer/ops/filesystem/ExpandArchive.java
+++ ops/core/src/main/java/org/platformlayer/ops/filesystem/ExpandArchive.java
@@ -8,9 +8,10 @@ import org.platformlayer.ops.Handler;
 import org.platformlayer.ops.OpsContext;
 import org.platformlayer.ops.OpsException;
 import org.platformlayer.ops.OpsTarget;
+import org.platformlayer.ops.tree.OpsTreeBase;
 
-public class ExpandArchive {
-	public File zipFile;
+public class ExpandArchive extends OpsTreeBase {
+	public File archiveFile;
 	public File extractPath;
 
 	@Handler
@@ -18,20 +19,23 @@ public class ExpandArchive {
 		if (OpsContext.isConfigure()) {
 			target.mkdir(extractPath);
 
-			String archiveFileName = zipFile.getName();
+			String archiveFileName = archiveFile.getName();
 
 			if (archiveFileName.endsWith(".zip")) {
 				// -u = update, for (something close to) idempotency
 				// -o = overwrite (no prompt)
-				Command unzipCommand = Command.build("unzip -u -o {0} -d {1}", zipFile, extractPath);
+				Command unzipCommand = Command.build("unzip -u -o {0} -d {1}", archiveFile, extractPath);
 				target.executeCommand(unzipCommand.setTimeout(TimeSpan.FIVE_MINUTES));
 			} else if (archiveFileName.endsWith(".tgz") || archiveFileName.endsWith(".tar.gz")) {
-				Command unzipCommand = Command.build("cd {0}; tar zxf {1}", extractPath, zipFile);
+				Command unzipCommand = Command.build("cd {0}; tar zxf {1}", extractPath, archiveFile);
 				target.executeCommand(unzipCommand.setTimeout(TimeSpan.FIVE_MINUTES));
-
 			} else {
 				throw new UnsupportedOperationException();
 			}
 		}
+	}
+
+	@Override
+	protected void addChildren() throws OpsException {
 	}
 }
