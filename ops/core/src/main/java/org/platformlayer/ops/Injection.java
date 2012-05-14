a7ca6da9dbd643a9cca63b3b27c3cf8ad7f42d43
Justin Santa Barbara <justin@fathomdb.com>
2012-05-13 18:29:02 -0700

    Allow easy injection of members

--- ops/core/src/main/java/org/platformlayer/ops/Injection.java
+++ ops/core/src/main/java/org/platformlayer/ops/Injection.java
@@ -4,4 +4,8 @@ public class Injection {
 	public static <T> T getInstance(Class<T> clazz) {
 		return OpsContext.get().getInjector().getInstance(clazz);
 	}
+
+	public static void injectMembers(Object instance) {
+		OpsContext.get().getInjector().injectMembers(instance);
+	}
 }
