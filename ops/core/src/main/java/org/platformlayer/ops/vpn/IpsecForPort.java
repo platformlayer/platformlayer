645e99a0c326581f2743ce4a942db50129e01103
Justin Santa Barbara <justin@fathomdb.com>
2012-06-09 22:29:46 -0700

    Fixed up SpdFile 'typos'

--- ops/core/src/main/java/org/platformlayer/ops/vpn/IpsecForPort.java
+++ ops/core/src/main/java/org/platformlayer/ops/vpn/IpsecForPort.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.openstack.utils.Utf8;
 import org.platformlayer.ops.Command;
+import org.platformlayer.ops.Handler;
 import org.platformlayer.ops.OpsException;
 import org.platformlayer.ops.filesystem.SyntheticFile;
 import org.platformlayer.ops.tree.OpsTreeBase;
@@ -11,7 +12,13 @@ import org.platformlayer.ops.tree.OpsTreeBase;
 public class IpsecForPort extends OpsTreeBase {
 	public int port;
 
-	class SpdFile extends SyntheticFile {
+	@Handler
+	public void handler() {
+	}
+
+	public static class SpdFile extends SyntheticFile {
+		public int port;
+
 		@Override
 		protected byte[] getContentsBytes() throws OpsException {
 			StringBuilder sb = new StringBuilder();
@@ -29,6 +36,7 @@ public class IpsecForPort extends OpsTreeBase {
 	protected void addChildren() throws OpsException {
 		SpdFile spd = addChild(SpdFile.class);
 		spd.filePath = new File("/etc/ipsec-tools.d/", "port-" + port + ".conf");
+		spd.port = port;
 
 		// Because we're just adding rules here, we can simply call setkey -f
 		spd.setUpdateAction(Command.build("setkey -f {0}", spd.filePath));
