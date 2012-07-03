fcc6bff75e8aab2a9fa0b12079c6e4ab7cb5800c
Justin Santa Barbara <justin@fathomdb.com>
2012-07-03 16:12:54 -0700

    Add (relatively useless) ServiceAccount formatter

---
+++ auth/keystone-cli/src/main/java/org/platformlayer/keystone/cli/formatters/ServiceAccountFormatter.java
@@ -0,0 +1,26 @@
+package org.platformlayer.keystone.cli.formatters;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import org.openstack.keystone.services.ServiceAccount;
+
+import com.fathomdb.cli.formatter.SimpleFormatter;
+import com.fathomdb.cli.output.OutputSink;
+import com.google.common.collect.Maps;
+
+public class ServiceAccountFormatter extends SimpleFormatter<ServiceAccount> {
+
+	public ServiceAccountFormatter() {
+		super(ServiceAccount.class);
+	}
+
+	@Override
+	public void visit(ServiceAccount o, OutputSink sink) throws IOException {
+		LinkedHashMap<String, Object> values = Maps.newLinkedHashMap();
+
+		values.put("ServiceAccount", o.toString());
+
+		sink.outputRow(values);
+	}
+}
