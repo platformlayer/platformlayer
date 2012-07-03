529fbecd7748c36a01f1bc4db29344e8ab7f26d1
Justin Santa Barbara <justin@fathomdb.com>
2012-07-03 16:09:51 -0700

    Throw if we try to PromoteToTyped with the wrong type

--- bindings/platformlayer-api/src/main/java/org/platformlayer/TypedItemMapper.java
+++ bindings/platformlayer-api/src/main/java/org/platformlayer/TypedItemMapper.java
@@ -8,6 +8,8 @@ import org.platformlayer.xml.JaxbHelper;
 import org.platformlayer.xml.XmlHelper.ElementInfo;
 import org.w3c.dom.Element;
 
+import com.google.common.base.Objects;
+
 public abstract class TypedItemMapper {
 
 	public <T> T promoteToTyped(UntypedItem untypedItem) throws OpsException {
@@ -25,6 +27,19 @@ public abstract class TypedItemMapper {
 		T typedItem;
 		try {
 			Element element = untypedItem.getDataElement();
+
+			String xmlElementName = jaxbHelper.getXmlElementName();
+			String nodeName = element.getLocalName();
+			if (!Objects.equal(xmlElementName, nodeName)) {
+				String type = element.getAttribute("xsi:type");
+
+				if (type != null && type.endsWith(":" + xmlElementName)) {
+					// OK
+				} else {
+					throw new OpsException("Incorrect element type: " + xmlElementName + " vs " + nodeName);
+				}
+			}
+
 			T object = jaxbHelper.unmarshal(element, itemClass);
 
 			if (!(object.getClass().isAssignableFrom(itemClass))) {
