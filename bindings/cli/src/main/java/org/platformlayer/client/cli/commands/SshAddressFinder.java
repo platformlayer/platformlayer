c34931a1a1135943b48e6abcbc9302acdf2e3dc3
Justin Santa Barbara <justin@fathomdb.com>
2012-07-05 19:14:44 -0700

    Be a bit smarter about finding SSH addresses in CLI

---
+++ bindings/cli/src/main/java/org/platformlayer/client/cli/commands/SshAddressFinder.java
@@ -0,0 +1,47 @@
+package org.platformlayer.client.cli.commands;
+
+import java.net.InetAddress;
+import java.util.List;
+import java.util.Set;
+
+import org.platformlayer.PlatformLayerClient;
+import org.platformlayer.PlatformLayerClientException;
+import org.platformlayer.UntypedItem;
+import org.platformlayer.core.model.Tag;
+import org.platformlayer.core.model.Tags;
+import org.platformlayer.xml.XmlHelper.ElementInfo;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+public class SshAddressFinder {
+	public final List<InetAddress> found = Lists.newArrayList();
+
+	final PlatformLayerClient client;
+
+	public SshAddressFinder(PlatformLayerClient client) {
+		super();
+		this.client = client;
+	}
+
+	public void visit(UntypedItem untypedItem) throws PlatformLayerClientException {
+		ElementInfo rootElementInfo = untypedItem.getRootElementInfo();
+
+		Set<String> instanceTypes = Sets.newHashSet();
+		instanceTypes.add("directInstance");
+		instanceTypes.add("googleCloudInstance");
+
+		if (instanceTypes.contains(rootElementInfo.elementName)) {
+			Tags itemTags = untypedItem.getTags();
+
+			for (InetAddress address : Tag.NETWORK_ADDRESS.find(itemTags)) {
+				found.add(address);
+			}
+		}
+
+		for (UntypedItem child : client.listChildren(untypedItem.getPlatformLayerKey())) {
+			visit(child);
+		}
+
+	}
+}
