735a0008fd9cf02e634c01975ea0c78c3fe370a7
Justin Santa Barbara <justin@fathomdb.com>
2012-07-10 11:57:19 -0700

    Refactoring user auth code; better injection

--- auth/server-user/src/main/java/org/openstack/keystone/server/KeystoneUserServer.java
+++ auth/server-user/src/main/java/org/openstack/keystone/server/KeystoneUserServer.java
@@ -3,7 +3,9 @@ package org.openstack.keystone.server;
 import java.io.File;
 import java.security.KeyStore;
 import java.util.EnumSet;
+import java.util.Map;
 
+import javax.inject.Inject;
 import javax.net.ssl.TrustManager;
 
 import org.eclipse.jetty.server.Connector;
@@ -14,15 +16,36 @@ import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.openstack.crypto.KeyStoreUtils;
+import org.openstack.keystone.resources.user.TokensResource;
 import org.platformlayer.WellKnownPorts;
 
+import com.google.common.collect.Maps;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceFilter;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.guice.JerseyServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
 public class KeystoneUserServer {
 	private Server server;
 
+	@Inject
+	GuiceServletConfig servletConfig;
+
 	public static void main(String[] args) throws Exception {
-		KeystoneUserServer server = new KeystoneUserServer();
+		Injector injector = Guice.createInjector(new GuiceAuthenticationConfig(), new JerseyServletModule() {
+			@Override
+			protected void configureServlets() {
+				bind(TokensResource.class);
+
+				Map<String, String> params = Maps.newHashMap();
+				params.put(PackagesResourceConfig.PROPERTY_PACKAGES, "org.codehaus.jackson.jaxrs");
+				serve("/*").with(GuiceContainer.class, params);
+			}
+		});
+
+		KeystoneUserServer server = injector.getInstance(KeystoneUserServer.class);
 
 		File keystoreFile = new File("keystore.jks");
 		String keystoreSecret = "notasecret";
@@ -42,26 +65,28 @@ public class KeystoneUserServer {
 	public void start(int port, KeyStore keystore, String keystorePassword) throws Exception {
 		this.server = new Server();
 
-		CustomTrustManagerSslContextFactory sslContextFactory = new CustomTrustManagerSslContextFactory();
+		{
+			CustomTrustManagerSslContextFactory sslContextFactory = new CustomTrustManagerSslContextFactory();
 
-		sslContextFactory.setKeyStore(keystore);
-		sslContextFactory.setKeyStorePassword(keystorePassword);
+			sslContextFactory.setKeyStore(keystore);
+			sslContextFactory.setKeyStorePassword(keystorePassword);
 
-		sslContextFactory.setWantClientAuth(true);
+			sslContextFactory.setWantClientAuth(true);
 
-		TrustManager[] trustManagers = new TrustManager[] { new AcceptAllClientCertificatesTrustManager() };
+			TrustManager[] trustManagers = new TrustManager[] { new AcceptAllClientCertificatesTrustManager() };
 
-		sslContextFactory.setTrustManagers(trustManagers);
+			sslContextFactory.setTrustManagers(trustManagers);
 
-		SslSelectChannelConnector connector = new SslSelectChannelConnector(sslContextFactory);
-		connector.setPort(port);
-		server.setConnectors(new Connector[] { connector });
+			SslSelectChannelConnector connector = new SslSelectChannelConnector(sslContextFactory);
+			connector.setPort(port);
+			server.setConnectors(new Connector[] { connector });
+		}
 
 		ServletContextHandler context = new ServletContextHandler();
 		context.setContextPath("/");
 		server.setHandler(context);
 
-		context.addEventListener(new UserServerConfig());
+		context.addEventListener(servletConfig);
 
 		// Must add DefaultServlet for embedded Jetty
 		// Failing to do this will cause 404 errors.
