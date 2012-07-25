901428742ae56f0e79a5b64dcd06d3e190451ff1
Justin Santa Barbara <justin@fathomdb.com>
2012-07-25 15:20:04 -0700

    Created listener hook so that if Jetty fails, we don't just sit there

---
+++ xaas/webapp/src/main/java/org/platformlayer/xaas/web/CloseOnFailLifecycleListener.java
@@ -0,0 +1,44 @@
+package org.platformlayer.xaas.web;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.eclipse.jetty.util.component.LifeCycle.Listener;
+
+public class CloseOnFailLifecycleListener implements Listener {
+
+	private static final Logger log = Logger.getLogger(CloseOnFailLifecycleListener.class);
+
+	@Override
+	public void lifeCycleStopping(LifeCycle event) {
+		log.info("lifeCycleStopping: " + event);
+	}
+
+	@Override
+	public void lifeCycleStopped(LifeCycle event) {
+		log.info("lifeCycleStopped: " + event);
+	}
+
+	@Override
+	public void lifeCycleStarting(LifeCycle event) {
+		log.info("lifeCycleStarting: " + event);
+	}
+
+	@Override
+	public void lifeCycleStarted(LifeCycle event) {
+		log.info("lifeCycleStarted: " + event);
+	}
+
+	@Override
+	public void lifeCycleFailure(LifeCycle event, Throwable cause) {
+		log.info("lifeCycleFailure: " + event, cause);
+
+		try {
+			log.info("Stopping server");
+			event.stop();
+		} catch (Exception e) {
+			log.error("Error while stopping server", e);
+			// TODO: System.exit?
+		}
+	}
+
+}
