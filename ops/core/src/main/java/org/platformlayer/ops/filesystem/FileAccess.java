ddd18c926284be6aa70eed6c37af37c99b423251
Justin Santa Barbara <justin@fathomdb.com>
2012-08-04 15:29:12 -0700

    Helper class for file owner + rights

---
+++ ops/core/src/main/java/org/platformlayer/ops/filesystem/FileAccess.java
@@ -0,0 +1,20 @@
+package org.platformlayer.ops.filesystem;
+
+public class FileAccess {
+	public static final FileAccess ROOT_OWNED_READABLE_DIRECTORY = new FileAccess("0755", "root", "root");
+
+	public final String mode;
+	public final String owner;
+	public final String group;
+
+	public FileAccess(String mode, String owner, String group) {
+		this.mode = mode;
+		this.owner = owner;
+		this.group = group;
+	}
+
+	public FileAccess(String mode) {
+		this(mode, null, null);
+	}
+
+}
