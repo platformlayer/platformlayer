6ab140fba2fe61b97cebaca3e8966f6d8169a122
Justin Santa Barbara <justin@fathomdb.com>
2012-08-10 17:44:04 -0700

    Support FileAccess overload

--- ops/core/src/main/java/org/platformlayer/ops/filesystem/FileAccess.java
+++ ops/core/src/main/java/org/platformlayer/ops/filesystem/FileAccess.java
@@ -2,6 +2,7 @@ package org.platformlayer.ops.filesystem;
 
 public class FileAccess {
 	public static final FileAccess ROOT_OWNED_READABLE_DIRECTORY = new FileAccess("0755", "root", "root");
+	public static final FileAccess ROOT_CAN_EXECUTE = new FileAccess("0700", "root", "root");
 
 	public final String mode;
 	public final String owner;
