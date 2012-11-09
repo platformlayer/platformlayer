c69832e4a686c4f9302a243db0fcf65acc0eb7f5
Justin Santa Barbara <justin@fathomdb.com>
2012-11-09 15:02:27 -0800

    Import the JAXB RI to try to get around the NamespacePrefixMapper problems

--- xaas/webapp/src/main/java/org/platformlayer/xaas/web/jaxrs/NamespacePrefixMapperImpl.java
+++ xaas/webapp/src/main/java/org/platformlayer/xaas/web/jaxrs/NamespacePrefixMapperImpl.java
@@ -1,7 +1,6 @@
 package org.platformlayer.xaas.web.jaxrs;
 
-public class NamespacePrefixMapperImpl extends com.sun.xml.internal.bind.marshaller.NamespacePrefixMapper {
-
+public class NamespacePrefixMapperImpl extends com.sun.xml.bind.marshaller.NamespacePrefixMapper {
 	@Override
 	public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
 		if (namespaceUri.equals("http://platformlayer.org/core/v1.0")) {
