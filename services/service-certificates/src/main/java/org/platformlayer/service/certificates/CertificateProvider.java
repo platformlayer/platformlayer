6d63cedd073d85285e0a791ca960e58754508c84
Justin Santa Barbara <justin@fathomdb.com>
2012-12-02 19:48:05 -0800

    Moving services-certificates to its own repo

--- services/service-certificates/src/main/java/org/platformlayer/service/certificates/CertificateProvider.java
+++
@@ -1,8 +0,0 @@
-package org.platformlayer.service.certificates;
-
-import org.platformlayer.ops.ServiceProviderBase;
-import org.platformlayer.xaas.Service;
-
-@Service("certificates")
-public class CertificateProvider extends ServiceProviderBase {
-}
