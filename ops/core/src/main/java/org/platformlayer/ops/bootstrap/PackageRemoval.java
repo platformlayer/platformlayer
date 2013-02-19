def3e15446d450f7d629e18ba90dc6d5f061b798
Justin Santa Barbara <justin@fathomdb.com>
2013-02-19 15:28:57 -0800

    Remove some more packages we don't want

--- ops/core/src/main/java/org/platformlayer/ops/bootstrap/PackageRemoval.java
+++ ops/core/src/main/java/org/platformlayer/ops/bootstrap/PackageRemoval.java
@@ -1,10 +1,11 @@
 package org.platformlayer.ops.bootstrap;
 
-import org.slf4j.*;
 import org.platformlayer.ops.Command;
 import org.platformlayer.ops.Handler;
 import org.platformlayer.ops.OpsException;
 import org.platformlayer.ops.OpsTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PackageRemoval {
 	@SuppressWarnings("unused")
@@ -21,25 +22,29 @@ public class PackageRemoval {
 		target.executeCommand(command);
 
 		// Maybe:
-		// apt-get remove groff-base
-		// apt-get install netcat6
-		// apt-get remove netcat-traditional
 
-		// apt-get remove consolekit
-		// apt-get remove sane-utils
+		command = Command
+				.build("apt-get remove --yes groff-base consolekit sane-utils  exim4-config installation-report");
+		target.executeCommand(command);
+
+		// Replace netcat with netcat6
+		target.executeCommand("apt-get install netcat6");
+		target.executeCommand("apt-get remove netcat-traditional");
 
 		// We just want headless...
-		// apt-get remove openjdk-7-jdk openjdk-7-jre
-		// apt-get remove icedtea-7-jre-cacao
-		// apt-get remove icedtea-7-jre-jamvm
+		command = Command
+				.build("apt-get remove --yes dbus openjdk-7-jdk openjdk-7-jre  icedtea-7-jre-cacao icedtea-7-jre-jamvm");
+		target.executeCommand(command);
 
 		// Do we need python?? Does it still get installed??
 		// apt-get remove python
 		// Warnings??
 		// apt-get remove install-info
-
+		//
 		// Squeeze:
 		// apt-get remove aptitude tasksel tasksel-data man-db manpages libxapian22 info
+
+		target.executeCommand("apt-get autoremove");
 
 	}
 }
