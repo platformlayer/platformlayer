17cc485489678ce383067acec66f03040d00404a
Justin Santa Barbara <justin@fathomdb.com>
2013-02-21 00:06:18 -0800

    Use LinkHelpers in auth

--- services/service-platformlayer/src/main/java/org/platformlayer/service/platformlayer/ops/auth/CommonAuthTemplateData.java
+++ services/service-platformlayer/src/main/java/org/platformlayer/service/platformlayer/ops/auth/CommonAuthTemplateData.java
@@ -4,25 +4,18 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.platformlayer.InetAddressChooser;
 import org.platformlayer.core.model.PlatformLayerKey;
 import org.platformlayer.ops.OpsException;
-import org.platformlayer.ops.databases.DatabaseHelper;
-import org.platformlayer.ops.helpers.InstanceHelpers;
 import org.platformlayer.ops.standardservice.StandardTemplateData;
+import org.platformlayer.ops.uses.LinkHelpers;
 import org.platformlayer.ops.uses.LinkTarget;
 import org.platformlayer.service.platformlayer.model.PlatformLayerAuthDatabase;
 
-import com.fathomdb.properties.PropertyUtils;
 import com.google.common.collect.Maps;
 
 public abstract class CommonAuthTemplateData extends StandardTemplateData {
-
 	@Inject
-	InstanceHelpers instanceHelpers;
-
-	@Inject
-	DatabaseHelper databases;
+	LinkHelpers links;
 
 	@Override
 	public void buildTemplateModel(Map<String, Object> model) throws OpsException {
@@ -43,14 +36,7 @@ public abstract class CommonAuthTemplateData extends StandardTemplateData {
 		Map<String, String> properties = Maps.newHashMap();
 
 		{
-			LinkTarget authDatabase = getAuthDatabase();
-
-			InetAddressChooser inetAddressChooser = InetAddressChooser.preferIpv6();
-
-			Map<String, String> config = authDatabase.buildLinkTargetConfiguration(inetAddressChooser);
-			config = PropertyUtils.prefixProperties(config, "auth.");
-
-			properties.putAll(config);
+			links.addTarget(properties, "auth.", getAuthDatabaseKey());
 		}
 
 		return properties;
