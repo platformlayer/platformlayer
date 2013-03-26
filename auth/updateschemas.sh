34ccb20b891f828090484ead503f51fb22f2f1bb
Justin Santa Barbara <justin@fathomdb.com>
2013-03-26 15:22:12 -0700

    Cleaning up XSD <-> Java

--- auth/updateschemas.sh
+++ auth/updateschemas.sh
@@ -3,11 +3,15 @@
 set -e
 
 # Generate the schema files
-mvn generate-resources
+pushd client
+mvn jaxb2:schemagen
+popd
 
 # Copy the generated schema(s)
 cp server-shared/target/generated-resources/schemagen/schema1.xsd client/src/main/schemas/keystone.xsd 
 
 # Update the bindings to the schema files
-mvn generate-resources
+pushd client
+mvn jaxb2:xjc
+popd
 
