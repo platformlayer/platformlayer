8476d29f23ae3cda188ab1253ef29ce8b7b673fb
Justin Santa Barbara <justin@fathomdb.com>
2013-03-28 18:14:26 -0700

    Remove unused imports

--- auth/server-user/src/main/java/org/openstack/keystone/resources/user/UserResourceBase.java
+++ auth/server-user/src/main/java/org/openstack/keystone/resources/user/UserResourceBase.java
@@ -1,26 +1,8 @@
 package org.openstack.keystone.resources.user;
 
-import java.util.Date;
-
-import javax.inject.Inject;
-
-import org.platformlayer.RepositoryException;
-import org.platformlayer.auth.ProjectEntity;
-import org.platformlayer.auth.UserEntity;
-import org.platformlayer.auth.keystone.AuthenticationSecrets;
-import org.platformlayer.auth.model.Access;
-import org.platformlayer.auth.model.Token;
 import org.platformlayer.auth.resources.PlatformlayerAuthResourceBase;
-import org.platformlayer.auth.services.TokenInfo;
-import org.platformlayer.auth.services.TokenService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.collect.Lists;
 
 public class UserResourceBase extends PlatformlayerAuthResourceBase {
-	private static final Logger log = LoggerFactory.getLogger(UserResourceBase.class);
-
-	
+	// private static final Logger log = LoggerFactory.getLogger(UserResourceBase.class);
 
 }
