99a9732d3a4dcdc0b049a8dbf248ae4afe4a31fb
Justin Santa Barbara <justin@fathomdb.com>
2013-04-01 17:04:38 -0700

    Automatically add dependencies to classpath in debug
    Simplifies launching in eclipse; workaround for a bug where dependencies don't have their maven dependencies included

--- xaas/webapp/src/main/java/org/platformlayer/xaas/web/PlatformLayerServer.java
+++ xaas/webapp/src/main/java/org/platformlayer/xaas/web/PlatformLayerServer.java
@@ -7,11 +7,13 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 public class PlatformLayerServer {
 	static final Logger log = LoggerFactory.getLogger(PlatformLayerServer.class);
@@ -38,6 +40,93 @@ public class PlatformLayerServer {
 					log.info("Added " + classesDir);
 				} catch (MalformedURLException e) {
 					throw new IllegalStateException("Error processing directory: " + classesDir, e);
+				}
+
+				File dependenciesDir = new File(serviceDir, "target/dependencies");
+				if (!dependenciesDir.isDirectory()) {
+					continue;
+				}
+				for (File file : dependenciesDir.listFiles()) {
+					String name = file.getName();
+					if (name.startsWith("fathomdb-"))
+						continue;
+
+					file = file.getAbsoluteFile();
+					try {
+						urls.add(file.toURI().toURL());
+						log.info("Added " + file);
+					} catch (MalformedURLException e) {
+						throw new IllegalStateException("Error processing directory: " + classesDir, e);
+					}
+				}
+
+			}
+		}
+
+		Set<String> done = Sets.newHashSet();
+
+		for (File serviceDirBase : serviceDirs) {
+			if (!serviceDirBase.exists()) {
+				throw new IllegalStateException("Directory not found: " + serviceDirBase);
+			}
+			// for Lists.newArrayList(classLoader.getURLs());
+			for (File serviceDir : serviceDirBase.listFiles()) {
+				if (!serviceDir.isDirectory()) {
+					continue;
+				}
+
+				File dependenciesDir = new File(serviceDir, "target/dependency");
+				if (!dependenciesDir.isDirectory()) {
+					continue;
+				}
+
+				List<String> prefixes = Lists.newArrayList();
+				prefixes.add("platformlayer-");
+				prefixes.add("fathomdb-");
+				prefixes.add("gwt-");
+				prefixes.add("logback-");
+				prefixes.add("jackson-");
+				// prefixes.add("jersey-");
+				prefixes.add("jaxb-");
+				prefixes.add("commons-");
+				prefixes.add("javax.inject-");
+				prefixes.add("validation-api-");
+				prefixes.add("guava-");
+				prefixes.add("gin-");
+				prefixes.add("guice-");
+				prefixes.add("junit-");
+				prefixes.add("httpclient-");
+				prefixes.add("httpcore-");
+				prefixes.add("mina-");
+				prefixes.add("sshd-");
+				prefixes.add("freemarker-");
+				prefixes.add("hibernate-");
+				prefixes.add("jboss-");
+				prefixes.add("service-");
+
+				for (File file : dependenciesDir.listFiles()) {
+					boolean veto = false;
+					String name = file.getName();
+					if (done.contains(name))
+						continue;
+
+					for (String prefix : prefixes) {
+						if (name.startsWith(prefix)) {
+							veto = true;
+							break;
+						}
+					}
+					if (veto)
+						continue;
+
+					file = file.getAbsoluteFile();
+					try {
+						urls.add(file.toURI().toURL());
+						log.info("Added " + file);
+						done.add(name);
+					} catch (MalformedURLException e) {
+						throw new IllegalStateException("Error processing directory: " + dependenciesDir, e);
+					}
 				}
 			}
 		}
