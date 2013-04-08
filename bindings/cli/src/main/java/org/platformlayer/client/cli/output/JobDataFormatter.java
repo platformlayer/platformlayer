b53a32ab8302b35afe1ccfec51c515949da16593
Justin Santa Barbara <justin@fathomdb.com>
2013-04-08 02:17:34 -0700

    Include job last run in CLI output

--- bindings/cli/src/main/java/org/platformlayer/client/cli/output/JobDataFormatter.java
+++ bindings/cli/src/main/java/org/platformlayer/client/cli/output/JobDataFormatter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 
 import org.platformlayer.jobs.model.JobData;
+import org.platformlayer.jobs.model.JobExecutionData;
 
 import com.fathomdb.cli.CliContext;
 import com.fathomdb.cli.formatter.SimpleFormatter;
@@ -24,6 +25,11 @@ public class JobDataFormatter extends SimpleFormatter<JobData> {
 		values.put("target", o.targetId);
 		values.put("action", o.action.getType());
 		// values.put("state", o.state);
+
+		JobExecutionData lastRun = o.getLastRun();
+		values.put("lastrun:id", lastRun != null ? lastRun.getExecutionId() : null);
+		values.put("lastrun:state", lastRun != null ? lastRun.getState() : null);
+		values.put("lastrun:end", lastRun != null ? lastRun.getEndedAt() : null);
 
 		sink.outputRow(values);
 	}
