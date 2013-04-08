c389d7615925c2eff0607b525fa9651d67e766de
Justin Santa Barbara <justin@fathomdb.com>
2013-04-08 02:06:58 -0700

    Enum parsing: null -> null

--- shared/model-shared/src/main/java/org/platformlayer/shared/EnumWithKeys.java
+++ shared/model-shared/src/main/java/org/platformlayer/shared/EnumWithKeys.java
@@ -3,6 +3,10 @@ package org.platformlayer.shared;
 public class EnumWithKeys {
 
 	public static <T extends EnumWithKey> T fromKey(Class<T> enumType, String key) {
+		if (key == null) {
+			return null;
+		}
+
 		// TODO: Put into map?
 		for (T enumValue : enumType.getEnumConstants()) {
 			if (enumValue.getKey().equals(key)) {
